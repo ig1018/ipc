b7abd44992e0ae0f48e73f91d4e78943b04ff3da
Irene Gonzalez <irenegonza0024@gmail.com>
2023-10-07 18:30:12 -0500

    Cargue inicial
    Proyecto IPC

---
+++ BackEnd_IPC/IPC.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IPC.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BackEnd_IPC/IPC.API/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IPC.API.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
